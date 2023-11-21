88a93dfdc14df69aa5c30ff3b84d4443a675ab90
Halil <halilgursoy91@gmail.com>
2023-11-21 15:06:47 -0500

    Fix compilation issue

--- Sources/FirebaseHelpers/Firestore/FirestoreModel.swift
+++ Sources/FirebaseHelpers/Firestore/FirestoreModel.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 public protocol FirestoreModel: Codable {
-    public static var collectionPath: String { get }
-    public static var secondaryIDKey: String { get }
+    static var collectionPath: String { get }
+    static var secondaryIDKey: String { get }
 }
